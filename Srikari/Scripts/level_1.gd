c0368fe83ca4101356d13514f84cdf697178e479
Srikari Veerubhotla <101363510+srikari78@users.noreply.github.com>
2024-03-23 14:18:32 -0700

    Add files via upload

---
+++ Srikari/Scripts/level_1.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Dungeon.tscn")
+	pass # Replace with function body.
